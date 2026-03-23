4bf1004fb03138f660b106faa7efa49f1da70094
Loric CANTINAT <loriccantinat@gmail.com>
2026-03-23 17:07:54 +0100

    Feature part 3

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -2,6 +2,7 @@ public class Program {
     //add of a comment to test git add and commit in a new branch
     public static void main(String[] args) {   
         //feature part2 commentary             
-        System.out.println("Hello, World!");                
+        System.out.println("Hello, World!");   
+        //feature part3 commentary             
     }                
 }
