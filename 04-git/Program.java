5f882b4d712ccd6900910691ceab96905cf21e1c
Loric CANTINAT <loriccantinat@gmail.com>
2026-03-23 17:26:39 +0100

    First change

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -1,7 +1,12 @@
 public class Program {                
     //add of a comment to test git add and commit in a new branch
+    public void method2() {
+        System.out.println("This is method 2");
+    }
+   
     public static void main(String[] args) {   
-        //feature part2 commentary             
+        //feature part2 commentary      
+        //this is a comment to test git       
         System.out.println("Hello, World!");   
         //feature part3 commentary             
     }                
